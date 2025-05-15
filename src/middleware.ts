c8589537b6fa08d965b6d0f2cea1349badb52fcf
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-05-15 23:10:14 +0100

    fixed the middleware issue in the code

---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth";
+import authConfig from "../auth.config";
+import { privateRoutes } from "../route";
+import { NextResponse } from "next/server";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth(async (req) => {
+  const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+  const url = 'http://localhost:3000';
+  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = nextUrl.pathname.includes("/login") || nextUrl.pathname.includes("/register");
+  const isApiRoute = nextUrl.pathname.startsWith("/api");
+
+  if (isApiRoute) {
+    return NextResponse.next();
+  }
+
+  if (isLoggedIn && isAuthRoute) {
+    return NextResponse.redirect(`${url}/dashboard`);
+  }
+
+  if (isAuthRoute && !isLoggedIn) {
+    return NextResponse.next();
+  }
+
+  if (!isLoggedIn && isPrivateRoute) {
+    return NextResponse.redirect(`${url}/login`);
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|static|.*\\..*|api|trpc|.well-known).*)",
+    "/"
+  ]
+};
